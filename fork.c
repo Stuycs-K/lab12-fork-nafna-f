0fb3814817818bfbc2eaf0637ac1a6d761261de6
Naf (Nafiyu Murtaza) <nmurtaza50@stuy.edu>
2024-11-25 14:50:17 -0500

    children worked on

--- fork.c
+++ fork.c
@@ -1,6 +1,34 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <time.h>
 int main(){
-    printf("test\n");
+    printf("%d about to create 2 child processes.\n", getpid());
+    int timeW;
+    pid_t p1;
+    p1 = fork();
+    if (p1 < 0){ //fork failed
+        perror("fork fail\n");
+        exit(1);
+    }else if (p1 == 0){ //child one
+        srand(getpid());
+        timeW = rand() % (5 - 1 + 1) + 1;
+        printf("%d %dsec\n", getpid(), timeW);
+    }else{ //create child two on parent
+        p1 = fork();
+        if (p1 < 0){ //fork failed
+            perror("fork fail\n");
+            exit(1);
+        }else if (p1 == 0){ //child two
+            srand(getpid());
+            timeW = rand() % (5 - 1 + 1) + 1;
+            printf("%d %dsec\n", getpid(), timeW);
+        }else{
+            //parent
+            printf("parent speaking\n");
+        }
+    }
     return 0;
 }
